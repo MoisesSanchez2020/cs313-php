1701633605fa54824cd15c9796364b781c69c269
MoisesSanchez2020 <san19039@byui.edu>
2020-10-22 23:44:37 -0600

    new project

---
+++ web/06_demo_note/courses.php
@@ -0,0 +1,56 @@
+<? php
+   require('dbConnect.php');
+$db = get_db();
+
+
+// FROM THE READING:
+/* $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); */
+
+
+    $query  = 'SELECT id, code, name FROM course';
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+   
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courses</title>
+</head>
+<body>
+
+      <h1>Courses</h1>
+
+       <ul>
+           <?PHP
+           foreach ($courses as $course)
+           {
+               $id = $course['id'];
+               $code = $course['code'];
+               $name = $course['name'];
+
+           echo "<li><p>$code - $name</p></li>";
+
+
+
+           }
+          
+          
+       ?>
+       </ul>
+    
+</body>
+</html>
+
