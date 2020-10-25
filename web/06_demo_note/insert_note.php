cf9afa458077639f591d487d730a2685859d8c48
MoisesSanchez2020 <san19039@byui.edu>
2020-10-24 21:31:00 -0600

    w

--- web/06_demo_note/insert_note.php
+++ web/06_demo_note/insert_note.php
@@ -1,19 +1,26 @@
 <?php
 
-$course_ProductID = htmlspecialchars($_POST['course_ProductID']);
-$content = htmlspecialchars($_POST['note_content']);
+   $course_id = htmlspecialchars($_POST['course_id']);
+   $content = htmlspecialchars($_POST['note_content']);
+ 
 
-require('dbConnect.php');
-$db = get_db();
 
-$stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
-$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
-$stmt->bindValue(':content', $content, PDO::PARAM_STR);
-$stmt->execute();
+   // echo "$course_id\n";
+   // echo $content;
 
-$new_page = "course_notes.php?course_id=$course_id";
 
-header("Location: $new_page");
-die();
+   require('dbConnect.php');
+   $db = get_db();
+   
+   // FROM THE READING:
+   $stmt = $db->prepare('INSERT INTO note(Fertilize_ProductID, ProductDescri) VALUES (:Fertilize_ProductID, :ProductDescription);');
+   $stmt->bindValue(':Fertilize_ProductID', $Fertilize_ProductID, PDO::PARAM_INT);
+   $stmt->bindValue(':ProductDescription', $ProductDescription, PDO::PARAM_STR);
+   $stmt->execute();
+   
+   $new_page = "course_note.php?Fertilize_ProductID=$Fertilize_ProductID";
+
+   header("Location: $new_page");
+   die();
 
 ?>
