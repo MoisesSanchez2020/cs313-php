ab96c87b93aab2761b1dc256cb723977845e0b07
MoisesSanchez2020 <san19039@byui.edu>
2020-10-24 21:36:36 -0600

    w

--- web/06_demo_note/insert_note.php
+++ web/06_demo_note/insert_note.php
@@ -1,26 +1,19 @@
 <?php
 
-   $course_id = htmlspecialchars($_POST['course_id']);
-   $content = htmlspecialchars($_POST['note_content']);
- 
+$course_id = htmlspecialchars($_POST['course_id']);
+$content = htmlspecialchars($_POST['note_content']);
 
+require('dbConnect.php');
+$db = get_db();
 
-   // echo "$course_id\n";
-   // echo $content;
+$stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
+$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->execute();
 
+$new_page = "course_notes.php?course_id=$course_id";
 
-   require('dbConnect.php');
-   $db = get_db();
-   
-   // FROM THE READING:
-   $stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
-   $stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
-   $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-   $stmt->execute();
-   
-   $new_page = "course_note.php?course_id=$course_id";
-
-   header("Location: $new_page");
-   die();
+header("Location: $new_page");
+die();
 
 ?>
