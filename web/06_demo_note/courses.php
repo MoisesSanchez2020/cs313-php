4535993be8b4380e447f1b3531f8788f268925e4
MoisesSanchez2020 <san19039@byui.edu>
2020-10-24 21:12:34 -0600

    w

--- web/06_demo_note/courses.php
+++ web/06_demo_note/courses.php
@@ -20,8 +20,9 @@ $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+   
     <link rel="stylesheet" href="demo.css">
+    <link rel="stylesheet" href="style.css">
     <title>IQGREEN</title>
 <body>
 <div id="container">
